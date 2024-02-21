66ec34d4bff8db9c19d0ce7556941eea9a7b701a
ukponotitus <titusukpono0@gmail.com>
2024-02-21 11:34:26 +0100

    folder setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import AdminLayOut from '@/pagelayout/adminlayout'
+import AdminLayOut from '@/layout/admin'
 import { Box } from '@mui/material'
 
 const inter = Inter({ subsets: ['latin'] })
